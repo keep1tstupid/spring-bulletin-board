2467721096f6b8a57a3e0c72d1f568ea07a1cca0
keep1tstupid <alexandra.globa@gmail.com>
2021-03-30 12:04:25 +0300

    tests for UserRepository added

--- src/test/java/com/example/bb/UserRepositoryTest.java
+++ src/test/java/com/example/bb/UserRepositoryTest.java
@@ -1,13 +1,50 @@
 package com.example.bb;
 
+import com.example.bb.domain.Role;
+import com.example.bb.domain.User;
 import com.example.bb.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+import static com.example.bb.domain.ERole.ROLE_USER;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 @SpringBootTest
 public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Test
+    public void findByUsernameTest() {
+        Optional<User> user = userRepository.findByUsername("test");
+        assertThat(user.isPresent());
+    }
 
+    @Test
+    public void createNewUserTest() {
+        User user = new User("test", "test@gmail.com", "$2y$12$0rRd4pqPE5hvGte36r3xgulgP40WwFsrqeXJcE8cmmm8y4lUwxfOW");
+
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(new Role(ROLE_USER));
+        user.setRoles(userRoles);
+        userRepository.save(user);
+
+        assertThat(user.getId()).isNotNull();
+    }
+
+    @Test
+    public void deleteUser() {
+        Optional<User> user = userRepository.findByUsername("user");
+        assertThat(user.isPresent());
+        user.ifPresent(u -> {
+            userRepository.deleteById(u.getId());
+        });
+        Optional<User> user2 = userRepository.findByUsername("user");
+        assertThat(user2.isEmpty());
+    }
 }
