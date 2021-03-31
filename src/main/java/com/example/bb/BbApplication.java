a691f68fe05efc4ab332b9e59faa201d7a453bf4
keep1tstupid <alexandra.globa@gmail.com>
2021-03-31 14:17:15 +0300

    one of attachment repository tests commented and test data from Application class removed to investigate heroku errors

--- src/main/java/com/example/bb/BbApplication.java
+++ src/main/java/com/example/bb/BbApplication.java
@@ -15,10 +15,6 @@ import java.util.Set;
 
 import static com.example.bb.domain.Role.*;
 
-// todo: add tests for controllers
-// todo: comments for complicated parts of project
-// todo: do nothing
-
 @SpringBootApplication
 public class BbApplication {
 
@@ -31,28 +27,6 @@ public class BbApplication {
                                     UserRepository userRepository
                                     //RoleRepository roleRepository
     ) {
-        return (args) -> {
-//            itemRepository.save(
-//                    new Item("leena", "Note", ItemType.NOTE, ItemState.APPROVED,
-//                            "description", "contact1"));
-//            itemRepository.save(
-//                    new Item("test", "I tired!", ItemType.COMPLAINT, ItemState.APPROVED,
-//                            "lorem ipsum", "contact2"));
-//            itemRepository.save(
-//                    new Item("rabjana", "One more bike for sale", ItemType.ADVERTISEMENT, ItemState.APPROVED,
-//                            "Selling almost new bike", "call me 123123123"));
-//
-//            User user1 = new User("adm", ROLE_ADMIN ,"adm@gmail.com", "$2y$12$0rRd4pqPE5hvGte36r3xgulgP40WwFsrqeXJcE8cmmm8y4lUwxfOW");
-//            userRepository.save(user1);
-//
-//            User user2 = new User("user", ROLE_USER,"user@gmail.com", "$2y$12$0rRd4pqPE5hvGte36r3xgulgP40WwFsrqeXJcE8cmmm8y4lUwxfOW");
-//            userRepository.save(user2);
-//
-//            User user3 = new User("mod", ROLE_MODERATOR, "mod@gmail.com", "$2y$12$0rRd4pqPE5hvGte36r3xgulgP40WwFsrqeXJcE8cmmm8y4lUwxfOW");
-//            userRepository.save(user3);
-//
-//            User user4 = new User("test", ROLE_MODERATOR, "test@gmail.com", "$2y$12$0rRd4pqPE5hvGte36r3xgulgP40WwFsrqeXJcE8cmmm8y4lUwxfOW");
-//            userRepository.save(user4);
-        };
+        return (args) -> { };
     }
 }
