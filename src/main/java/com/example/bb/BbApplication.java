a20c6cc3b3912205f84adaf346373cc3434d45d1
keep1tstupid <alexandra.globa@gmail.com>
2021-04-02 13:16:16 +0300

    unused import removed, demo method commented, env variables updated

--- src/main/java/com/example/bb/BbApplication.java
+++ src/main/java/com/example/bb/BbApplication.java
@@ -1,19 +1,12 @@
 package com.example.bb;
 
-import com.example.bb.domain.*;
 import com.example.bb.repository.ItemRepository;
-//import com.example.bb.repository.RoleRepository;
 import com.example.bb.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
-import static com.example.bb.domain.Role.*;
 
 @SpringBootApplication
 public class BbApplication {
@@ -22,11 +15,10 @@ public class BbApplication {
         SpringApplication.run(BbApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner bbDemo(ItemRepository itemRepository,
-                                    UserRepository userRepository
-                                    //RoleRepository roleRepository
-    ) {
-        return (args) -> { };
-    }
+//    @Bean
+//    public CommandLineRunner bbDemo(ItemRepository itemRepository,
+//                                    UserRepository userRepository
+//    ) {
+//        return (args) -> { };
+//    }
 }
