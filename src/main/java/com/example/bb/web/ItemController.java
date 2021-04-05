224980c9dae967b501f7ebe10fdba8ffd5865413
keep1tstupid <alexandra.globa@gmail.com>
2021-04-05 22:14:10 +0300

    user controller addded

--- src/main/java/com/example/bb/web/ItemController.java
+++ src/main/java/com/example/bb/web/ItemController.java
@@ -4,7 +4,6 @@ import com.example.bb.domain.Item;
 import com.example.bb.domain.ItemType;
 import com.example.bb.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
---
+++ src/main/java/com/example/bb/web/UserController.java
@@ -0,0 +1,26 @@
+package com.example.bb.web;
+
+
+import com.example.bb.domain.User;
+import com.example.bb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    //get all users
+    @GetMapping("/users")
+    public @ResponseBody
+    List<User> allUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+}
