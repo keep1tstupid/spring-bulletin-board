c60fb3ce46d911ee0848e8d6e01d9ba3c13dbf3c
keep1tstupid <alexandra.globa@gmail.com>
2021-04-14 20:55:57 +0300

    saving file process updated

--- src/main/java/com/example/bb/web/AttachmentController.java
+++ src/main/java/com/example/bb/web/AttachmentController.java
@@ -10,8 +10,6 @@ import com.example.bb.service.AttachmentStorageService;
 
 import com.google.cloud.storage.*;
 
-import org.apache.tomcat.util.http.fileupload.FileItemStream;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,12 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Part;
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -48,72 +42,105 @@ public class AttachmentController {
     @Autowired
     private Storage storage;
 
-    private void checkFileExtension(String fileName) throws ServletException {
+    private boolean checkFileExtension(String fileName) throws ServletException {
         if (fileName != null && !fileName.isEmpty() && fileName.contains(".")) {
-            String[]allowedExt = {".jpg", ".jpeg", ".png", ".gif"};
+            String[] allowedExt = {".jpg", ".jpeg", ".png", ".gif"};
             for (String ext:allowedExt) {
                 if(fileName.endsWith(ext)) {
-                    return;
+                    return true;
                 }
             }
             throw new ServletException("file must be an image");
         }
+        return false;
     }
-
-        private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
-    }
-
 
     // method to add new file and link it to appropriate item
     @PostMapping("/api/upload")
     public ResponseEntity<ResponseMessage> uploadFile(
             //@RequestParam("file")
             MultipartFile file,
-            @RequestParam Long id) {
+            @RequestParam Long id) throws ServletException {
+
         String message = "";
-        try {
-            Attachment current = storageService.store(file);
-            checkFileExtension(current.getName());
-            message = "Uploaded the file successfully: " + file.getOriginalFilename() + " Assigned id: " +
-                    current.getId();
+        String fileName = file.getOriginalFilename();
+        String bucketName = "spring-bucket-test-lolka";
 
-            String cwd = Paths.get("").toAbsolutePath().toString();
-            File newFile = new File(cwd + "/files/" + file.getOriginalFilename());
-            newFile.createNewFile();
+        // if format of file is ok
+        if (checkFileExtension(fileName)) {
 
+            //go and try to save in cloud
+            String storageFileName = fileName;
+            try {
+                BlobInfo blobInfo = storage.create(
+                        BlobInfo.newBuilder(bucketName, storageFileName).setContentType("image/jpeg").build(),
+                        file.getBytes()
+                );
+                System.out.println(blobInfo.getMediaLink());
+            } catch (Exception e) {
+                System.out.println(e);
+                message = "Problem occurred during saving to the cloud";
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
 
-            // test cloud
-            String bucketName="spring-bucket-test-lolka";
-            String storageFileName = current.getName();
-            InputStream fileStream = new FileInputStream(convertMultiPartToFile(file));
+            // if uploaded to cloud, go and save in db
+            try {
+                Attachment current = storageService.store(file);
+                message = "Saved in DB successfully: " + fileName + " Assigned id: " +
+                        current.getId();
 
-            BlobInfo blobInfo = storage.create(BlobInfo.newBuilder(bucketName, storageFileName)
-                            .setAcl(new ArrayList<>(Arrays.asList(Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER))))
-                            .build(), fileStream);
+                // saving locally
+//                String cwd = Paths.get("").toAbsolutePath().toString();
+//                File newFile = new File(cwd + "/files/" + file.getOriginalFilename());
+//                newFile.createNewFile();
 
-            System.out.println(blobInfo.getMediaLink());
+                // update item with attachment Id
+                Optional<Item> targetItem = itemRepository.findById(id);
+                targetItem.ifPresent(item -> {
+                    item.setAttachmentId(current.getId());
+                    itemRepository.save(item);
+                });
 
-            Optional<Item> targetItem = itemRepository.findById(id);
-            targetItem.ifPresent(item -> {
-                item.setAttachmentId(current.getId());
-                itemRepository.save(item);
-            });
+//                FileOutputStream fout = new FileOutputStream(newFile);
+//                fout.write(file.getBytes());
+//                fout.close();
 
-            FileOutputStream fout = new FileOutputStream(newFile);
-            fout.write(file.getBytes());
-            fout.close();
-
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception e) {
-            message = "Could not upload the file: " + file.getOriginalFilename() + "!" + e.getMessage();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+                return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+            } catch (Exception e) {
+                message = "Could not save the file: " + file.getOriginalFilename() + "!" + e.getMessage();
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
         }
+
+        message = "Uploading failed during format check";
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
     }
+
+//        try {
+//            Attachment current = storageService.store(file);
+//            message = "Uploaded the file successfully: " + fileName + " Assigned id: " +
+//                    current.getId();
+//
+//            String cwd = Paths.get("").toAbsolutePath().toString();
+//            File newFile = new File(cwd + "/files/" + file.getOriginalFilename());
+//            newFile.createNewFile();
+//
+//            Optional<Item> targetItem = itemRepository.findById(id);
+//            targetItem.ifPresent(item -> {
+//                item.setAttachmentId(current.getId());
+//                itemRepository.save(item);
+//            });
+//
+//            FileOutputStream fout = new FileOutputStream(newFile);
+//            fout.write(file.getBytes());
+//            fout.close();
+//
+//            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+//        } catch (Exception e) {
+//            message = "Could not upload the file: " + file.getOriginalFilename() + "!" + e.getMessage();
+//            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+//        }
+//    }
 
     // get all files
     @GetMapping("/api/files")
