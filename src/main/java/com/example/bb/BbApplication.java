045fe928b543ab58acf3500acd9da102da770e27
keep1tstupid <alexandra.globa@gmail.com>
2021-04-19 11:29:32 +0300

    unused imports removed

--- src/main/java/com/example/bb/BbApplication.java
+++ src/main/java/com/example/bb/BbApplication.java
@@ -1,11 +1,7 @@
 package com.example.bb;
 
-import com.example.bb.repository.ItemRepository;
-import com.example.bb.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
