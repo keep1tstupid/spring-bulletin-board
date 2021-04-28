4123fc1005454e1b909862b667e88615c3ed3ecb
keep1tstupid <alexandra.globa@gmail.com>
2021-04-28 18:08:22 +0300

    added possibility to get all user roles

--- src/main/java/com/example/bb/web/UserController.java
+++ src/main/java/com/example/bb/web/UserController.java
@@ -1,11 +1,14 @@
 package com.example.bb.web;
 
 import com.example.bb.domain.Item;
+import com.example.bb.domain.ItemType;
+import com.example.bb.domain.Role;
 import com.example.bb.domain.User;
 import com.example.bb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -20,6 +23,14 @@ public class UserController {
     public @ResponseBody
     List<User> allUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+    // get all roles
+    @GetMapping("/roles")
+    public @ResponseBody
+    List<Role> allRoles() {
+        Role[] roles = Role.values();
+        return Arrays.asList(roles);
     }
 
     // add new user
